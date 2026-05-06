93b1d66d13464945a2d89c519200f413dfe0551f
sabateange-maker <sabateange@gmail.com>
2026-05-06 11:34:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Portfolio de Ángeles cargado");
+
+// scroll suave
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
